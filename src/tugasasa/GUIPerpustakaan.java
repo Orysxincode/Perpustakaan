a851bb3e14a62c8d94a22c2897591844e84027b1
Orysxincode <thedes57@gmail.com>
2022-12-17 16:52:05 +0700

    Program Perpustakaan

--- src/tugasasa/GUIPerpustakaan.java
+++ src/tugasasa/GUIPerpustakaan.java
@@ -5,17 +5,13 @@
 package tugasasa;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
 /**
